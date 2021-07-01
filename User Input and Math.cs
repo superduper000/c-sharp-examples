6db332f79478d6f81d3d8c814e1f0a42de3fafcb
GTA extremeX <67494757+GTAextremeX@users.noreply.github.com>
2021-07-01 21:37:45 +0530

    Update User Input and Math.cs

--- User Input and Math.cs
+++ User Input and Math.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
- namespace User_Input
+ namespace UserInput
  {
    public class Program
    {
