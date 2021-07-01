997a56b2a26b09707a0a9edd9d2958c5ea2219f8
GTA extremeX <67494757+GTAextremeX@users.noreply.github.com>
2021-07-01 21:36:48 +0530

    Update User Input and Math.cs

--- User Input and Math.cs
+++ User Input and Math.cs
@@ -2,10 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
- // Compiler version 4.0, .NET Framework 4.5
-
-
- namespace Dcoder
+ namespace User_Input
  {
    public class Program
    {
